44dc0ba3ad65e1d11630057be596931ef1a826ea
Wolfseverest93 <j_bump.silvette2011@yahoo.com>
2018-01-11 15:50:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content == '+Cleanup') {
-        if (message.member.roles.some(r=>["Leader-kun! <3", "Knights of the Hammer!"].includes(r.name)) ) {
+        if (message.member.roles.some(r=>["Big Bad Wolf!", "Knights of the Hammer!"].includes(r.name)) ) {
             message.reply('Beginning cleanup module!');
             console.log('Beginning cleanup module!');
             CY1 = 1
@@ -43,7 +43,7 @@ client.on('message', message => {
 
 //  Cycle One Functions
 
-when CY1 == 1 {
+while CY1 == 1 {
     if (message.deletable) {
         message.delete()
             .then(msg => console.log(`Deleted message from ${msg.author}`))
@@ -54,210 +54,12 @@ when CY1 == 1 {
         CY1 = 0
     }
 });
-when CY1 == 2 {
+while CY1 == 2 {
     if (message.deletable) {
         message.delete()
             .then(msg => console.log(`Deleted message from ${msg.author}`))
             .catch(console.error);
-        CY1 = 3
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 3 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 4
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 4 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 5
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 5 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 6
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 6 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 7
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 7 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 8
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 8 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 9
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 9 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 10
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 10 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 11
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 11 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 12
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 12 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 13
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 13 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 14
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 14 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 15
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 15 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 16
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 16 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 17
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 17 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 18
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 18 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 19
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 19 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 20
-    } else {
-        console.log('No further messages to remove!')
-        CY1 = 0
-    }
-});
-when CY1 == 20 {
-    if (message.deletable) {
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        CY1 = 0
+        CY1 = 1
     } else {
         console.log('No further messages to remove!')
         CY1 = 0
