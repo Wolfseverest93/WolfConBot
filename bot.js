a9ca4f62424aececdc9d98d990b48dbab3fb275d
Wolfseverest93 <j_bump.silvette2011@yahoo.com>
2018-01-12 04:53:05 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,14 +36,17 @@ client.on('message', message => {
 //  Message Delete Loop  //
 
 function MassDelete1() {
-    if channel.fetchMessages({limit: 1}) {
-        .then(msg => (message.deletable))
-            .then(msg => message.delete(1))
+    if (channel.fetchMessages({limit: 1})) {
+        if (msg => (message.deletable)) {
+            msg => message.delete(1)
                 .then(msg => console.log('Deleted message from ${msg.author}'))
                 .catch(console.error);
             setTimeout(() => {
                 MassDelete2();
             }, 10);
+        } else {
+            console.log('Failed to delete message! Pinned or locked?')
+        }
     } else {
         console.log('Failed to fetch messages! Maybe there are none?')
     }
@@ -51,14 +54,18 @@ function MassDelete1() {
             
 
 function MassDelete2() {
-    if channel.fetchMessages({limit: 1}) {
-        .then(msg => (message.deletable))
-            .then(msg => message.delete(1))
+    if (channel.fetchMessages({limit: 1})) {
+        if (msg => (message.deletable)) {
+            msg => message.delete(1)
                 .then(msg => console.log('Deleted message from ${msg.author}'))
                 .catch(console.error);
             setTimeout(() => {
                 MassDelete1();
             }, 10);
+        } else {
+            console.log('Failed to delete message! Pinned or locked?')
+        }
     } else {
         console.log('Failed to fetch messages! Maybe there are none?')
+    }
 }
